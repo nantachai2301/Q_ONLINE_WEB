fd0bf9c441bdea08ee35745c3560274ed531c9fe
kwanticha <634259001@webmail.npru.ac.th>
2023-09-17 13:03:08 +0700

    new

--- src/view/authorities/Walkin/Showdata.js
+++ src/view/authorities/Walkin/Showdata.js
@@ -507,7 +507,7 @@ function Showdata() {
             id="BookingWalkinpageSize"
             activePage={page}
             itemsCountPerPage={pageSize}
-            totalItemsCount={pageData.length}
+            totalItemsCount={user.length}
             pageRangeDisplayed={5}
             onChange={setPage}
           />
