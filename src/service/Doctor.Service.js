b8a9f3354e2c54941772d6e0325df5209f8dd10e
kwanticha <634259001@webmail.npru.ac.th>
2023-10-18 16:32:22 +0700

    .

--- src/service/Doctor.Service.js
+++ src/service/Doctor.Service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/apis/";
+const API_URL = "https://wild-rose-clam-wig.cyclic.app/apis/.";
 
 const getDoctor = async ( ) => {
   return await axios.get(API_URL + "doctors",);
